2e53a6c902d3ee18ae338b188804598eaca0d00b
Vitor Melo <vitormelodev@gmail.com>
2026-05-29 16:26:34 -0300

    calloc

--- libft.h
+++ libft.h
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stddef.h>
+# include <stdlib.h>
 
 /*
 ** Character checks
@@ -53,6 +54,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 /*
 ** Conversion
